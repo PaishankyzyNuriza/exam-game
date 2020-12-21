750fbce137523625e72fada1dd9032d496d8d447
Nuriza Paishan kyzy <73305001+PaishankyzyNuriza@users.noreply.github.com>
2020-12-21 17:11:16 +0500

    Update block.py

--- block.py
+++ block.py
@@ -2,6 +2,7 @@ from pygame import *
 width = 20
 height = 20
 
+
 class Plat(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
@@ -9,10 +10,12 @@ class Plat(sprite.Sprite):
         self.image = image.load('grass.png')
         self.rect = Rect(x, y, width, height)
 
+
 class Spike(Plat):
     def __init__(self, x, y):
         Plat.__init__(self, x, y)
         self.image = image.load('saw.png')
+
 
 class Trophey(Plat):
     def __init__(self, x, y):
