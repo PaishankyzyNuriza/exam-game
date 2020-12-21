4ce6bae4e39c14c66dbbb7487f0c996a6fb4b3d9
tamer-3 <73985106+tamer-3@users.noreply.github.com>
2020-12-21 17:05:24 +0600

    Add files via upload

---
+++ Igrok.py
@@ -0,0 +1,79 @@
+from pygame import *
+import block
+from game import screen
+import pygame
+win = False
+speed = 8
+width = 20
+height = 20
+jump = 10
+gravitation = 0.9
+
+class Igrok(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.speedX = 0
+        self.speedY = 0
+        self.naZemle = False
+        self.startX = x
+        self.startY = y
+        self.image = Surface((width, height))
+        self.image = image.load('alienGreen_stand_.png')
+        self.rect = Rect(x, y, width, height)
+
+    def update(self, levo, pravo, verh, platform):
+        if verh:
+            if self.naZemle:
+                self.speedY = -jump
+
+        if levo:
+            self.speedX = -speed
+
+        if pravo:
+            self.speedX = speed
+
+        if not (pravo or levo):
+            self.speedX = 0
+
+        if not self.naZemle:
+            self.speedY = self.speedY + gravitation
+
+        self.naZemle = False
+        self.rect.y = self.rect.y + self.speedY
+        self.collision(0, self.speedY, platform)
+        self.rect.x = self.rect.x + self.speedX
+        self.collision(self.speedX, 0, platform)
+
+    def collision(self, speedX, speedY, platform):
+        for n in platform:
+            if sprite.collide_rect(self, n):
+                if isinstance(n, block.Spike):
+                    self.death()
+                if isinstance(n, block.Trophey):
+                    win = True
+                    if win == True:
+                        t = pygame.font.Font(None, 80)
+                        text = t.render('You Win!!!', 3, (0, 0, 0))
+                        screen.blit(text, (250, 250))
+                if speedX > 0:
+                    self.rect.right = n.rect.left
+
+                if speedX < 0:
+                    self.rect.left = n.rect.right
+
+                if speedY > 0:
+                    self.rect.bottom = n.rect.top
+                    self.naZemle = True
+                    self.speedY = 0
+
+                if speedY < 0:
+                    self.rect.top = n.rect.bottom
+                    self.speedY = 0
+
+    def death(self):
+        time.wait(600)
+        self.teleport(self.startX, self.startY)
+
+    def teleport(self, X, Y):
+        self.rect.x = X
+        self.rect.y = Y
---
+++ game.py
@@ -0,0 +1,112 @@
+import pygame
+from pygame import *
+from Igrok import *
+from block import *
+
+
+WIDTH = 800
+HEIGHT = 600
+DISPLAY = (WIDTH, HEIGHT)
+Platform_wid = 20
+Platform_hei = 20
+Platform_block = (Platform_wid, Platform_hei)
+screen = pygame.display.set_mode(DISPLAY)
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Platformer")
+    BG = pygame.image.load('colored_land.png')
+    screen.blit(BG, (0, 0))
+    pygame.display.flip()
+
+    player = Igrok(25, 560)
+    levo = pravo = False
+    verh = False
+
+    hitGround = pygame.sprite.Group()
+    platform = []
+    hitGround.add(player)
+    level1 = [
+        "----------------------------------------",
+        "-                                      -",
+        "-                                      -",
+        "-              ---     --             *-",
+        "-        ------       -     --    ------",
+        "-                                      -",
+        "------                           /     -",
+        "-                                      -",
+        "-------------///------------     -------",
+        "-                                      -",
+        "-                                      -",
+        "-                    -------------------",
+        "-                                      -",
+        "---      ------   ----                 -",
+        "-                     -  ------      ---",
+        "-      ---             -               -",
+        "-                       -         --   -",
+        "- --/--                   -            -",
+        "-      /                   /           -",
+        "-       /                              -",
+        "--       -------               -       -",
+        "-                   -------            -",
+        "-                                      -",
+        "--                              --------",
+        "-                                      -",
+        "-                 -----                -",
+        "----------///-----     -----///-----   -",
+        "-                                      -",
+        "-                                      -",
+        "-------------------///------------------"]
+    x = 0
+    y = 0
+
+    for j in level1:
+        for k in j:
+            if k == "-":
+                plat = Plat(x, y)
+                hitGround.add(plat)
+                platform.append(plat)
+            if k == "/":
+                spike = Spike(x, y)
+                hitGround.add(spike)
+                platform.append(spike)
+            if k == "*":
+                star = Trophey(x, y)
+                hitGround.add(star)
+                platform.append(star)
+            x = x + Platform_wid
+        y = y + Platform_hei
+        x = 0
+
+    s = True
+    while s:
+        pygame.time.Clock().tick(30)
+        for q in pygame.event.get():
+            if q.type == QUIT:
+                s = False
+            if q.type == KEYDOWN and q.key == K_LEFT:
+                levo = True
+            if q.type == KEYDOWN and q.key == K_RIGHT:
+                pravo = True
+            if q.type == KEYUP and q.key == K_LEFT:
+                levo = False
+            if q.type == KEYUP and q.key == K_RIGHT:
+                pravo = False
+            if q.type == KEYDOWN and q.key == K_UP:
+                verh = True
+            if q.type == KEYUP and q.key == K_UP:
+                verh = False
+        pygame.display.update()
+        screen.blit(BG, (0, 0))
+        hitGround.draw(screen)
+        player.update(levo, pravo, verh, platform)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
